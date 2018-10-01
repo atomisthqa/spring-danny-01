84d5cb743e901c6cbbcf2291114860fc75f66d13
Christian Dupuis <cd@atomist.com>
2018-10-01 22:25:41 +0200

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/SpringDanny01Application.java
@@ -0,0 +1,12 @@
+package com.atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDanny01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDanny01Application.class, args);
+	}
+}
